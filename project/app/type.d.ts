700ad9e056fcf12b828b07189482402231d86962
MasatoTakakusaki <mtkksk1780@gmail.com>
2024-08-31 16:31:00 +0700

    Mark all user locations

--- project/app/type.d.ts
+++ project/app/type.d.ts
@@ -2,3 +2,18 @@ type LocationType = {
   lat: number;
   lng: number;
 };
+
+type UserLocationType = {
+  locationName: {
+    city: string | null;
+    region: string | null;
+    country: string | null;
+  };
+  latitude: number;
+  longitude: number;
+  count: number;
+};
+
+type LocationListProps = {
+  locations: UserLocationType[];
+};
